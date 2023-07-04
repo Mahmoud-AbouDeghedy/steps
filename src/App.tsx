618d0612f3994b534a550f432bee0a6f96fcbfe6
Mahmoud Ehab AbouDeghedy <mahmoudehab1000@gmail.com>
2023-07-04 09:49:13 +0300

    simple react project with tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+const messages = [
+	"Learn React ⚛️",
+	"Apply for jobs 💼",
+	"Invest your new income 🤑",
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [step, setStep] = useState(1);
+	const [isOpened, setIsOpened] = useState(true);
+
+	const handleNext = () => {
+		setStep((prev) => (prev < 3 ? prev + 1 : prev));
+	};
+
+	const handlePrevious = () => {
+		setStep((prev) => (prev > 1 ? prev - 1 : prev));
+	};
+
+	return (
+		<>
+			<button className="close" onClick={() => setIsOpened((s) => !s)}>
+				&times;
+			</button>
+			{isOpened && (
+				<div className="steps">
+					<div className="numbers">
+						<div className={step >= 1 ? "active" : ""}>1</div>
+						<div className={step >= 2 ? "active" : ""}>2</div>
+						<div className={step >= 3 ? "active" : ""}>3</div>
+					</div>
+					<p className="message">
+						Step {step}: {messages[step - 1]}
+					</p>
+					<div className="buttons">
+						<button
+							style={{ backgroundColor: "#7950f2", color: "#fff" }}
+							onClick={handlePrevious}
+						>
+							Previous
+						</button>
+						<button
+							style={{ backgroundColor: "#7950f2", color: "#fff" }}
+							onClick={handleNext}
+						>
+							Next
+						</button>
+					</div>
+				</div>
+			)}
+		</>
+	);
 }
 
 export default App;
